01c4c7fd59239b6e358bf8e83a396d98cfc0ae09
Abdurahman-Abdella <abdellahayan645@gmail.com>
2025-06-23 18:19:09 +0300

    update-confJs

--- conf.js
+++ conf.js
@@ -10,10 +10,11 @@ const albumSpan = document.querySelector(".album-author");
 const texts = document.querySelectorAll(".text");
 const albumNum = document.querySelector(".album-num");
 const spotifyWidget = document.querySelector(".spotify-widget iframe");
-const albums = [
+
+  const albums = [
   {
     album: "Timeless",
-		author: "The Weekend",
+    author: "The Weekend",
     emblem: "Life is better with music",
     "bg-color": ["#e0b86f", "#0D1827"],
     "accent-color": "#e0b86f",
@@ -22,7 +23,7 @@ const albums = [
   },
   {
     album: "One of the girls",
-		author: "The Weekend",
+    author: "The Weekend",
     emblem: "Life is better with music",
     "bg-color": ["#c72f2f", "#0D1827"],
     "accent-color": "#c72f2f",
@@ -31,8 +32,7 @@ const albums = [
   },
   {
     album: "Starboy",
-		author: "The Weekend",
-
+    author: "The Weekend",
     emblem: "Life is better with music",
     "bg-color": ["#ffbd44", "#0D1827"],
     "accent-color": "#ffbd44",
@@ -41,30 +41,28 @@ const albums = [
   },
   {
     album: "Blinding lights",
-		author: "The Weekend",
-
+    author: "The Weekend",
     emblem: "Life is better with music",
     "bg-color": ["#580f41", "#0D1827"],
     "accent-color": "#a72a6b",
     url: "images/blindin.jpg",
     spotify: "https://open.spotify.com/embed/track/0VjIjW4GlUZAMYd2vXMi3b?utm_source=generator"
   },
-
-	  {
+  {
     album: "Standin next to You",
     author: "Jungkook",
     emblem: "Life is better with music",
     "bg-color": ["#1db954", "#0D1827"],
-    "accent-color": "#1db954",
-    url: "images/jungkook.jpg", 
+    "accent-color": "#0a9396",
+    url: "images/jungkook.jpg",
     spotify: "https://open.spotify.com/embed/track/2KslE17cAJNHTsI2MI0jb2?utm_source=generator"
   },
   {
     album: "NINAO",
     author: "Gims",
     emblem: "Life is better with music",
-    "bg-color": ["#1db954", "#0D1827"],
-    "accent-color": "#1db954",
+    "bg-color": ["#bb3e03", "#0D1827"],
+    "accent-color": "#ee9b00",
     url: "images/gims.jpg",
     spotify: "https://open.spotify.com/embed/track/2uBKQbVcw8G9m34lGYM6VA?utm_source=generator"
   },
@@ -72,14 +70,14 @@ const albums = [
     album: "The Search",
     author: "NF",
     emblem: "Life is better with music",
-    "bg-color": ["#1db954", "#0D1827"],
-    "accent-color": "#1db954",
+    "bg-color": ["#14213d", "#0D1827"],
+    "accent-color": "grey",
     url: "images/nate.jpg",
     spotify: "https://open.spotify.com/embed/track/3oLe5ZILASG8vU5dxIMfLY?utm_source=generator"
   },
   {
     album: "Stay",
-    author: "Justin beiber",
+    author: "Justin Bieber",
     emblem: "Life is better with music",
     "bg-color": ["#1db954", "#0D1827"],
     "accent-color": "#1db954",
@@ -87,25 +85,25 @@ const albums = [
     spotify: "https://open.spotify.com/embed/track/567e29TDzLwZwfDuEpGTwo?utm_source=generator"
   },
   {
-    album: "Thats what i like",
-    author: "Bruno mars",
+    album: "That's What I Like",
+    author: "Bruno Mars",
     emblem: "Life is better with music",
-    "bg-color": ["#1db954", "#0D1827"],
-    "accent-color": "#1db954",
+    "bg-color": ["#3a86ff", "#0D1827"],
+    "accent-color": "#00b4d8",
     url: "images/mars.jpg",
     spotify: "https://open.spotify.com/embed/track/0KKkJNfGyhkQ5aFogxQAPU?utm_source=generator"
   },
   {
     album: "Uptown Funk",
-    author: "Bruno mars",
+    author: "Bruno Mars",
     emblem: "Life is better with music",
-    "bg-color": ["#1db954", "#0D1827"],
-    "accent-color": "#1db954",
+    "bg-color": ["#ff006e", "#0D1827"],
+    "accent-color": "#fb5607",
     url: "images/uptown.jpg",
     spotify: "https://open.spotify.com/embed/track/32OlwWuMpZ6b0aN2RZOeMS?utm_source=generator"
   }
-
 ];
+
 
 
 scrollLeft.addEventListener("click", () => handleClickScroll(-1));
